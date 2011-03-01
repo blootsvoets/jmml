1438476162958d15515ee9f596ff9184c208fe1e
Joris Borgdorff <j.borgdorff@uva.nl>
2011-03-01 15:42:35 +0100

    Completed an SIRange

--- src/eu/mapperproject/xmml/util/SIRange.java
+++ src/eu/mapperproject/xmml/util/SIRange.java
@@ -8,5 +8,29 @@ package eu.mapperproject.xmml.util;
  *
  */
 public class SIRange {
+	private final SIUnit min, max;
+	
+	public SIRange(SIUnit min, SIUnit max) {
+		if (min.compareTo(max) > 0) {
+			throw new IllegalArgumentException("Maximum in a range must be larger than the minimum");
+		}
+		this.min = min;
+		this.max = max;
+	}
+	
+	public SIRange(SIUnit value) {
+		this(value, value);
+	}
+	
+	public SIUnit getMinimum() {
+		return this.min;
+	}
 
+	public SIUnit getMaximum() {
+		return this.max;
+	}
+	
+	public boolean isPoint() {
+		return this.min.equals(this.max);
+	}
 }
