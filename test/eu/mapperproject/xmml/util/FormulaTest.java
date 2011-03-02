b30ac6415b3fbe41173bfe41140dfcbd5d220139
Joris Borgdorff <j.borgdorff@uva.nl>
2011-03-02 13:37:48 +0100

    Formula test: pow test and better error handling

--- test/eu/mapperproject/xmml/util/FormulaTest.java
+++ test/eu/mapperproject/xmml/util/FormulaTest.java
@@ -2,6 +2,7 @@ package eu.mapperproject.xmml.util;
 
 import static org.junit.Assert.*;
 
+import java.text.ParseException;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
@@ -17,17 +18,20 @@ public class FormulaTest {
 	private Map<String,Integer> xyGive;
 	@Before
 	public void setUp() {
-		try {
 		x = 2d;
 		y = 3d;
-		xy = Formula.parse("x*y");
-		log = Formula.parse("log(x)");
-		plus = Formula.parse("y*y+x*y-x+x*log(x)-sqrt(x)-sqrt(y)");
-		pow = Formula.parse("x^2");
-		sizeof = Formula.parse("x*sizeof(double)");
-		parensA = Formula.parse("x*sizeof(double) + (x+y)/-x");
-		parensB = Formula.parse("((x+x)*3 + (x+y)/x)");
-		} catch (Exception e) {};
+		
+		try {
+			xy = Formula.parseFormula("x*y");
+			log = Formula.parseFormula("log(x)");
+			plus = Formula.parseFormula("y*y+x*y-x+x*log(x)-sqrt(x)-sqrt(y)");
+			pow = Formula.parseFormula("x^2");
+			sizeof = Formula.parseFormula("x*sizeof(double)");
+			parensA = Formula.parseFormula("x*sizeof(double) + (x+y)/-x");
+			parensB = Formula.parseFormula("((x+x)*3 + (x+y)/x)");
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 		
 		xySet = new TreeSet<String>();
 		xySet.add("x"); xySet.add("y");
@@ -45,6 +49,7 @@ public class FormulaTest {
 	public void values() {
 		assertEquals(new Double(x*y), new Double(xy.evaluate(xyGive)));
 		assertEquals(new Double(x*64), new Double(sizeof.evaluate(xyGive)));
+		assertEquals(new Double(Math.pow(x, 2d)), new Double(pow.evaluate(xyGive)));
 		assertEquals(new Double(y*y+x*y-x+x*Math.log(x)-Math.sqrt(x)-Math.sqrt(y)), new Double(plus.evaluate(xyGive)));
 		assertEquals(new Double(x*64+(x+y)/-x), new Double(parensA.evaluate(xyGive)));
 		assertEquals(new Double(((x+x)*3 + (x+y)/x)), new Double(parensB.evaluate(xyGive)));
