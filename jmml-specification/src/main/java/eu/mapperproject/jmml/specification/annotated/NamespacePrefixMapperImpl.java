e7682e13bc6725a0eaa09e706cb7e35fc19a9f12
Joris Borgdorff <j.borgdorff@uva.nl>
2011-08-16 12:08:26 +0200

    Removed print statement in namespace mapper

--- jmml-specification/src/main/java/eu/mapperproject/jmml/specification/annotated/NamespacePrefixMapperImpl.java
+++ jmml-specification/src/main/java/eu/mapperproject/jmml/specification/annotated/NamespacePrefixMapperImpl.java
@@ -11,7 +11,6 @@ public class NamespacePrefixMapperImpl extends NamespacePrefixMapper {
 
 	@Override
 	public String getPreferredPrefix(String namespaceUri, String suggestion, boolean requirePrefix) {
-		System.out.println(namespaceUri);
 		//lets make the xmml namespace the default one
 		if ("".equals(namespaceUri)) {
 			Logger.getLogger(NamespacePrefixMapperImpl.class.getCanonicalName()).fine("Certain non-xMML elements are not marked as transient.");
